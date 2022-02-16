d99981d33db5e295566d55c974ebfa0e31ce8d90
Mabdille <mabdille97@gmail.com>
2022-02-16 18:10:29 +0000

    Made router to allow the user to navigate between different pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Route, BrowserRouter as Router, Routes, Link } from "react-router-dom"
+import HomePage from "./components/HomePage"
 import HomeScreen from "./components/HomeScreen";
-
 const App = () => {
   return (
     <div className="App">
       <h1 className='text-info  text-center'>
-      <HomeScreen/> 
       </h1>
+      
+      <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/homescreen">Quran</Link>
+          </li>
+          </ul>
+      </nav>
+          <Routes>
+              <Route path="/" exact element={<HomePage/>} />
+              <Route path="/homescreen" element={<HomeScreen/>} />
+          </Routes>
+      </Router>
     </div>
   );
 }
