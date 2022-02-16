84cac7ced2db5f7879b83b9c89cab662ca745db4
Mabdille <mabdille97@gmail.com>
2022-02-16 18:50:19 +0000

    nav bar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,19 @@
+import './HomePage.css'
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='div'>
+            <nav>
+                <div class="topnav">
+                    <a class="active" href="#home">Home</a>
+                    <a href="#news">Quran</a>
+                    <a href="#contact">Tafseer</a>
+                    <a href="#about">Hadith</a>
+                </div>
+            </nav>
+        
+            <div>
+                <h1 className='title'>myQuran</h1>
+            </div>
         </div>
         
     )
